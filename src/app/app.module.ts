9767e7c25f2f546beda474e40bb8eaf7a8d97384
Nikola Peevski <nip@simplyso.com>
2017-09-06 15:14:39 +0200

    (type:chore) Add Angular material, HammerJs and Constants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import { httpWrapperService } from './shared/httpWrapper.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    httpWrapperService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/Constants.ts
@@ -0,0 +1,4 @@
+export class Constants {
+  public static EXAMPLE_CONSTANT = 'EXAMPLE_CONSTANT';
+  public static APIURL = 'https://api.tripbook.dk/';
+}
