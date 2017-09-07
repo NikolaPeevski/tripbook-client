89e1073e21a72ac50618dc48de0e434b4c3bb53b
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-09-07 20:35:20 +0200

    automatic deployment

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+# Clean previous build
+rm -rf dist
+# Create a prod build
+npm run build prod
+# ZIP the dist folder
+tar czf dist.tar.gz dist/*
+# Copy the zipped file to the server
+scp -i id_tripbook_deploy_rsa dist.tar.gz deploy@188.166.94.245:~
+# scp -i ~/.ssh/id_tripbook_deploy_rsa dist.tar.gz deploy@188.166.94.245:~
+# Remove the zipped folder locally
+rm dist.tar.gz
+# Connect to the server
+ssh -i id_tripbook_deploy_rsa deploy@188.166.94.245 << 'ENDSSH'
+# ssh -i ~/.ssh/id_tripbook_deploy_rsa deploy@188.166.94.245 << 'ENDSSH'
+# Delete the previous version
+rm -rf tripbook-client
+# Unzip the file
+tar xf dist.tar.gz
+# Rename 'dist' folder to 'tripbook-client'
+mv -T dist tripbook-client
+# Remove the zipped folder
+rm dist.tar.gz
+# Open the folder
+cd tripbook-client
+# End the connection
+ENDSSH
+
